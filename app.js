c9cc64fa531c0134b6510adc7eb20363b62c9aca
Daniel2711 <detaddeo.d@gmail.com>
2021-02-08 20:27:10 +0100

    Add API restcountry

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ new Vue({
         searchInput: 'pippo',
 
         // array film from API
-        movies: []
+        movies: [],
+
+        // array country
+        countries: []
     },
 
     methods: {
@@ -30,11 +33,45 @@ new Vue({
                             movie.poster_path = 'https://image.tmdb.org/t/p/original'+movie.poster_path;
                         }
                     });
-                    
                 })
 
                 .catch(error =>{
                     console.log('Error in the API call');
+                    console.log(error);
+                })
+
+            axios
+                .get('https://api.themoviedb.org/3/search/movie', {params:{
+                    api_key: '7d986f5d7f72343a109e093583f2df92',
+                    query: this.searchInput,
+                }})
+                
+                .then(dataAPI =>{
+                    console.log(dataAPI.data)
+                    this.movies = dataAPI.data.results
+                    console.log(this.movies[0])
+
+                    this.movies.forEach((movie,index) => {
+                        if(movie.poster_path != null){
+                            movie.poster_path = 'https://image.tmdb.org/t/p/original'+movie.poster_path;
+                        }
+                    });                    
+                })
+
+                .catch(error =>{
+                    console.log('Error in the API movies');
+                    console.log(error);
+                })
+
+            axios
+                .get('https://restcountries.eu/rest/v2/all?fields=name;flag;alpha2Code')
+                .then(apiState =>{
+                    console.log(apiState.data)
+                    this.countries = apiState.data
+                })
+
+                .catch(error => {
+                    console.log('Error in the API country');
                     console.log(error);
                 })
         }
